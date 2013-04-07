52f2bca74d4e2514c2df190826b3ca741396eb76
dabing1022 <dabing1022@gmail.com>
2013-04-07 14:50:52 +0800

    Initial Commit

---
+++ NiuNiu/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NiuNiu
+//
+//  Created by childhood on 13-4-7.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
