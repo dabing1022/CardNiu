3a3c6a7caa4a16a7c4d1e6c6154a84ed0a099c1e
CHILDHOOD <dabing1022@gmail.com>
2013-04-22 19:50:31 +0800

    enter reading cards
    enter reading cards

---
+++ NiuNiu/ReadingCardsLayer.h
@@ -0,0 +1,13 @@
+//
+//  ReadingCardsLayer.h
+//  NiuNiu
+//
+//  Created by childhood on 13-4-22.
+//
+//
+
+#import "CCLayer.h"
+
+@interface ReadingCardsLayer : CCLayer
+
+@end
---
+++ NiuNiu/ReadingCardsLayer.m
@@ -0,0 +1,13 @@
+//
+//  ReadingCardsLayer.m
+//  NiuNiu
+//
+//  Created by childhood on 13-4-22.
+//
+//
+
+#import "ReadingCardsLayer.h"
+
+@implementation ReadingCardsLayer
+
+@end
